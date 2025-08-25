2e9b6c8bdc1f2d3c1c9b0fd0789750bf2ec847a6
varanasisreelakshmi1-mintu <varanasisreelakshmi1@gmail.com>
2025-08-25 15:18:40 +0530

    Update Samplename.java

--- Samplename.java
+++ Samplename.java
@@ -2,5 +2,7 @@ public class Samplename{
     public static void main(String[] args) {
         String name="sree lakshmi";
         System.out.println("Hello ,my name is "+ name);
+        String rollno = "23D21A05J0";
+        System.out.println("My roll number is "+ rollno);
     }
 }
